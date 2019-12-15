067b61a14bea7c5400df18a103c2e44e601ead83
enewman00 <eenewman@clockwork-solutions.com>
2019-12-14 23:01:04 -0600

    prettied it up, added a default picture for the weather card (planning on more later), added animations to the text input, the button, and the card creation

--- js/button.js
+++ js/button.js
@@ -1,12 +1,40 @@
+//weather types
+//"clear sky" - "Clear"
+//"broken clouds" - "Clouds"
+//"overcast clouds" - "Clouds"
+//
+
+
+//background image pseudocode (https://openweathermap.org/weather-conditions)
+//if it begins with 2xx - thunderstorm
+// if it begins with 3xx - drizzle
+// 5xx - rain
+// 6xx - snow
+// 7xx - atmosphere
+// 800 - clear
+// 80x - clouds
 function getTemperature()
 {
     const city = document.getElementById("city-input").value;
     var temp;
-    // const temp;
+
+    //set text of the city entered
+    document.getElementById("location").innerHTML = city + "";
+
+    //set background image
+    document.getElementById("squareOne").style.backgroundImage = "url('pictures/clouds.jpg')";
+
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city + "&APPID=23d18f8ee7da3a481d47d75065594755&units=imperial")
         .then(response => response.json())
-        .then(json => document.getElementById("temp-output").innerHTML = json.main.temp);
+        .then(json => document.getElementById("temperatureId").innerHTML = parseInt(json.main.temp) + "&#176"); //degree symbol
+
     // console.log(response.json().main.temp);
     //document.getElementById("temp-output").innerHTML = JSON.stringify(temp);
             //.json();
+    animateButton();
+}
+
+function animateButton()
+{
+    document.getElementById('squareOne').className = 'classname';
 }
